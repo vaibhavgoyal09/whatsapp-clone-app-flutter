79876a95f936a91d195f163a6d5dd85bac7a6b52
vaibhavgoyal09 <79615436+vaibhavgoyal09@users.noreply.github.com>
2022-06-14 17:35:21 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/mystikcoder/whatsapp_clone_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mystikcoder.whatsapp_clone_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class ScrollGlowEffect extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Whatsapp',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: "SFProText"
+      ),
+      builder: (context, child) {
+        return ScrollConfiguration(behavior: ScrollGlowEffect(), child: child!);
+      }
+    );
+  }
+}
